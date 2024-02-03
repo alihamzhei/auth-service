13e906de00b2112172d7a6f923dc5cfe543f71b4
alihamzehei <alihamzehei2017@gmail.com>
2024-02-03 17:52:46 +0330

    add response service

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        App\Providers\ResponseServiceProvider::class,
         /*
          * Application Service Providers...
          */
